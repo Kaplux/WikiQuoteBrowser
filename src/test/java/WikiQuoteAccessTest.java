86d618907c0083e8a553981c747dcff3455daa45
ippon <ippon@ippon-laptop.(none)>
2012-03-06 10:44:18 +0100

    maj tu

--- src/test/java/WikiQuoteAccessTest.java
+++ src/test/java/WikiQuoteAccessTest.java
@@ -1,24 +1,22 @@
 import java.io.IOException;
-import java.util.List;
+
+import junit.framework.TestCase;
 
 import org.apache.http.client.ClientProtocolException;
-import org.junit.Test;
 
 import fr.wikiQuoteBrowser.WikiQuoteAccess;
 
 
-public class WikiQuoteAccessTest {
+public class WikiQuoteAccessTest extends TestCase{
 
-	@Test
-	public void test() throws ClientProtocolException, IOException {
+	public void testSearchQuote() throws Exception {
 		WikiQuoteAccess wiki=new WikiQuoteAccess("http://en.wikiquote.org/w/api.php");
     	String searchCriteria= "cleese";
     	String[] result;
 		result = wiki.searchQuote(searchCriteria);
 	}
-	
-	@Test
-	public void test2() throws ClientProtocolException, IOException {
+
+	public void testgetQuotePage() throws ClientProtocolException, IOException {
 		WikiQuoteAccess wiki=new WikiQuoteAccess("http://en.wikiquote.org/w/api.php");
     	String searchCriteria= "Main%20Page";
     	String result;
